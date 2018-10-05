fcf75fa5d7e2b4f7dde3b48a8faf52b4e29591bf
Praveen Shukla <praveen.shukla@go-jek.com>
2018-10-05 10:30:16 +0530

    remove checksum

--- Formula/proctor.rb
+++ Formula/proctor.rb
@@ -2,12 +2,13 @@ class Proctor < Formula
   desc "Proctor CLI"
   homepage "https://github.com/gojektech/proctor"
   url "https://github.com/gojektech/proctor/archive/v0.1.0.tar.gz"
-  sha256 "220f7de43afbbadd599cc810c4252fb794f3bbad374ff4417ba4cf13932cf45e"
   head "https://github.com/gojektech/proctor.git"
 
   depends_on "go" => :build
 
   def install
+    system "make", "setup"
+    system "make", "install"
     system "make", "install"
   end
 
